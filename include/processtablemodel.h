31ce0d42e5a5f4dbaa08b70dcea7659c97fd4587
SandroWissmann <sandro.wissmann@googlemail.com>
2020-06-13 15:29:45 +0200

    feat: adds ProcessTableModel to manage processes

---
+++ include/processtablemodel.h
@@ -0,0 +1,37 @@
+#ifndef PROCESSTABLEMODEL_H
+#define PROCESSTABLEMODEL_H
+
+#include <QAbstractTableModel>
+
+#include <memory>
+
+namespace Sysmon{
+
+class Process;
+
+class ProcessTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+public:
+    ProcessTableModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+
+protected:
+    void timerEvent(QTimerEvent *event) override;
+
+private:
+    QVector<std::shared_ptr<Process>> updateProcesses();
+
+    QVector<std::shared_ptr<Process>> mProcesses = {};
+};
+
+QString toFormatedTime(long seconds);
+
+QString fillWithNLeadingSigns(const QString& s, int minSize, char sign);
+
+}
+
+#endif // PROCESSTABLEMODEL_H
